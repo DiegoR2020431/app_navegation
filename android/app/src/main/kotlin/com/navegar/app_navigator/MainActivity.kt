143398de63e4d57b7821881ade4efe7a65eaa2bb
DiegoR2020431 <103212071+DiegoR2020431@users.noreply.github.com>
2022-04-07 14:17:13 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/navegar/app_navigator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.navegar.app_navigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/principal.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Start'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Services"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('To Return!'),
+        ),
+      ),
+    );
+  }
+}
+
